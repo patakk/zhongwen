2369afd980f8f31a8017a2db8c28106f64b175bc
patakk <paolo.ceric@gmail.com>
2024-10-19 23:39:58 +0200

    refactor

--- convert_jsons_to_db.py
+++ convert_jsons_to_db.py
@@ -34,6 +34,9 @@ def migrate_json_to_db(delete_files=False):
         if filename.endswith('.json'):
             file_path = os.path.join(json_dir, filename)
             username = os.path.splitext(filename)[0]  # Username is the filename without extension
+
+            if username not in ['贝海', 'beihai', 'triksi', 'Shuyu']:
+                continue
             
             with open(file_path, 'r', encoding='utf-8') as f:
                 data = json.load(f)
