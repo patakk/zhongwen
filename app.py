5d323621a0e027f11b6eb78c9b6660c0c8a9d0bc
patakk <paolo.ceric@gmail.com>
2025-04-04 00:47:49 +0200

    test push

--- app.py
+++ app.py
@@ -66,6 +66,8 @@ limiter = Limiter(
     app=app,
 )
 
+
+
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 logger.info("Application root directory: " + app.config['APPLICATION_ROOT'])
