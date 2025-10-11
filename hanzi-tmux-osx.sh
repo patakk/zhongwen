a5e4513d20a9cbae278157f78fcc52ea05683e81
Paolo Čerić <paolo.ceric@microblink.com>
2025-10-11 20:53:09 +0200

    chore: tmux

---
+++ hanzi-tmux-osx.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+tmux new-session -d -s zhongweb
+
+# Backend (Flask)
+tmux send-keys -t zhongweb 'cd /Users/paolo/zhongwen/hanzi-backend' C-m
+sleep 0.3
+tmux send-keys -t zhongweb 'poetry run python app.py' C-m
+
+# Frontend (Vue)
+tmux split-window -v -t zhongweb
+tmux send-keys -t zhongweb.1 'cd /Users/paolo/zhongwen/hanzi-frontend' C-m
+sleep 0.3
+tmux send-keys -t zhongweb.1 'npm run dev' C-m
+
+# Search backend
+tmux split-window -h -t zhongweb.1
+tmux send-keys -t zhongweb.2 'cd /Users/paolo/zhongwen/hanzi-search' C-m
+sleep 0.3
+tmux send-keys -t zhongweb.2 'poetry run python app.py' C-m
+
+tmux select-pane -t zhongweb.0
+tmux attach-session -t zhongweb
