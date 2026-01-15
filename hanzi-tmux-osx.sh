4486a91409107e118373e63c1db5ae49b74cff11
Paolo Čerić <paolo.ceric@microblink.com>
2026-01-15 05:18:33 +0100

    pre build

--- hanzi-tmux-osx.sh
+++ hanzi-tmux-osx.sh
@@ -3,19 +3,19 @@
 tmux new-session -d -s zhongweb
 
 # Backend (Flask)
-tmux send-keys -t zhongweb 'cd /Users/paolo/hanzilab/repo/hanzi-backend' C-m
+tmux send-keys -t zhongweb 'cd /Users/paolo/programming/hanzilab/repo/hanzi-backend' C-m
 sleep 0.3
 tmux send-keys -t zhongweb 'poetry run python app.py' C-m
 
 # Frontend (Vue)
 tmux split-window -v -t zhongweb
-tmux send-keys -t zhongweb.1 'cd /Users/paolo/hanzilab/repo/hanzi-frontend' C-m
+tmux send-keys -t zhongweb.1 'cd /Users/paolo/programming/hanzilab/repo/hanzi-frontend' C-m
 sleep 0.3
 tmux send-keys -t zhongweb.1 'npm run dev' C-m
 
 # Search backend
 tmux split-window -h -t zhongweb.1
-tmux send-keys -t zhongweb.2 'cd /Users/paolo/hanzilab/repo/hanzi-search' C-m
+tmux send-keys -t zhongweb.2 'cd /Users/paolo/programming/hanzilab/repo/hanzi-search' C-m
 sleep 0.3
 tmux send-keys -t zhongweb.2 'poetry run python app.py' C-m
 
